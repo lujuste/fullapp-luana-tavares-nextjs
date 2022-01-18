0304f3013997fec756036d1ea6d5cbb3cce5fbf0
Lucas Juste <lu.scatollini@hotmail.com>
2022-01-17 21:40:56 -0300

    new

--- src/pages/peticoes/index.tsx
+++ src/pages/peticoes/index.tsx
@@ -8,7 +8,7 @@ export default function Petitions() {
   return (
     <>
       <Header />
-      <Flex flexDir="column" w="100%" h="100%" maxW="1400" mx="auto" mb="2rem">
+      <Flex flexDir="column" w="100%" h="80vh" maxW="1400" mx="auto" mb="2rem">
         <Flex
           mx="auto"
           flexDir="column"
@@ -34,7 +34,7 @@ export default function Petitions() {
             Petições
           </Heading>
 
-          <NextLink href="/peticoes/devastacao-mata-atlantica">
+          <NextLink href="https://luanatavaressp.com.br/">
             <Flex
               align="center"
               my="1.5rem"
@@ -52,8 +52,8 @@ export default function Petitions() {
                 _hover={{ color: 'pink.500' }}
                 cursor="pointer"
               >
-                Assine para ajudar a salvar o meio ambiente e acabar com os
-                desmatamentos na amazônia
+                Inscreva-se para participar da Conecta: aceleradora de Mulheres
+                na política.
               </Text>
               <Text
                 fontSize={['14px', '14px', '16px']}
@@ -61,73 +61,8 @@ export default function Petitions() {
                 mt="0.5rem"
                 mr="auto"
               >
-                <Icon mt="-0.3rem" mr="0.2rem" align="center" as={TimeIcon} /> 2
-                de jan de 2022
-              </Text>
-            </Flex>
-          </NextLink>
-
-          <NextLink href="/peticoes/devastacao-mata-atlantica">
-            <Flex
-              align="center"
-              my="1.5rem"
-              mx="auto"
-              flexDir="column"
-              maxW={['300px', '300px', '800px']}
-            >
-              <Text
-                maxW="1000px"
-                fontWeight={'bold'}
-                fontSize={['1.2rem', '1.2rem', '2rem']}
-                fontFamily={'Raleway'}
-                mx="auto"
-                textAlign="left"
-                _hover={{ color: 'pink.500' }}
-                cursor="pointer"
-              >
-                Assine para ajudar a salvar o meio ambiente e acabar com os
-                desmatamentos na amazônia
-              </Text>
-              <Text
-                fontSize={['14px', '14px', '16px']}
-                align="center"
-                mt="0.5rem"
-                mr="auto"
-              >
-                <Icon mt="-0.3rem" mr="0.2rem" align="center" as={TimeIcon} /> 2
-                de jan de 2022
-              </Text>
-            </Flex>
-          </NextLink>
-          <NextLink href="/peticoes/devastacao-mata-atlantica">
-            <Flex
-              align="center"
-              my="1.5rem"
-              mx="auto"
-              flexDir="column"
-              maxW={['300px', '300px', '800px']}
-            >
-              <Text
-                maxW="1000px"
-                fontWeight={'bold'}
-                fontSize={['1.2rem', '1.2rem', '2rem']}
-                fontFamily={'Raleway'}
-                mx="auto"
-                textAlign="left"
-                _hover={{ color: 'pink.500' }}
-                cursor="pointer"
-              >
-                Assine para ajudar a salvar o meio ambiente e acabar com os
-                desmatamentos na amazônia
-              </Text>
-              <Text
-                fontSize={['14px', '14px', '16px']}
-                align="center"
-                mt="0.5rem"
-                mr="auto"
-              >
-                <Icon mt="-0.3rem" mr="0.2rem" align="center" as={TimeIcon} /> 2
-                de jan de 2022
+                <Icon mt="-0.3rem" mr="0.2rem" align="center" as={TimeIcon} />{' '}
+                17 de jan de 2022
               </Text>
             </Flex>
           </NextLink>

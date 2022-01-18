f1d69ce9b8962f7113c186f70d4c62df9244c98e
Lucas Juste <lu.scatollini@hotmail.com>
2022-01-18 20:18:14 -0300

    newww

--- src/pages/novidades/[slug].tsx
+++ src/pages/novidades/[slug].tsx
@@ -290,7 +290,7 @@ export default function Post({ post, navigation }: PostProps) {
                     {navigation.nextPost[0].data.title}{' '}
                   </Heading>
                 </Link>
-                <Link href={`/novidades/${navigation.nextPost[0].data.uid}`}>
+                <Link href={`/novidades/${navigation.nextPost[0].uid}`}>
                   <Text
                     mt="1rem"
                     color="black"
